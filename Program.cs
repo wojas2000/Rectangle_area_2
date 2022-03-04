ea1f43010bd933292d5bb53c41a8b586ad460bca
wojas2000 <wojas2000@o2.pl>
2022-03-04 07:01:20 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Rectangle_area_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, pole;
+            Console.WriteLine("Program liczy pole prostokąta.");
+            Console.WriteLine("Podaj długość dłuższego boku:");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj długość krótszego boku:");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Pole prostokąta wynosi: {pole = a * b}");
+        }
+    }
+}
